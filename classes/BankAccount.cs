163c24c0dbfaac8da59caa6fec780a59912f0e5d
FateevNIX <aleksandr.fateev@nixsolutions.com>
2020-12-11 18:08:56 +0200

    added Acounts classes

--- classes/BankAccount.cs
+++ classes/BankAccount.cs
@@ -23,17 +23,22 @@ namespace classes
             }
         }
 
-        public BankAccount(string name, decimal initialBalance)
+        private readonly decimal minimumBalance;
+
+        public BankAccount(string name, decimal initialBalance) : this(name, initialBalance, 0) { }
+
+        public BankAccount(string name, decimal initialBalance, decimal minimumBalance)
         {
-            this.Owner = name;
-
-            MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
-
             this.Number = accountNumberSeed.ToString();
             accountNumberSeed++;
+
+            this.Owner = name;
+            this.minimumBalance = minimumBalance;
+            if (initialBalance > 0)
+                MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
         }
 
-
+        public virtual void PerformMonthEndTransactions() { }
 
         public void MakeDeposit(decimal amount, DateTime date, string note)
         {
@@ -51,12 +56,23 @@ namespace classes
             {
                 throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
             }
-            if (Balance - amount < 0)
+            var overdraftTransaction = CheckWithdrawalLimit(Balance - amount < minimumBalance);
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
+            if (overdraftTransaction != null)
+                allTransactions.Add(overdraftTransaction);
+        }
+
+        protected virtual Transaction? CheckWithdrawalLimit(bool isOverdrawn)
+        {
+            if (isOverdrawn)
             {
                 throw new InvalidOperationException("Not sufficient funds for this withdrawal");
             }
-            var withdrawal = new Transaction(-amount, date, note);
-            allTransactions.Add(withdrawal);
+            else
+            {
+                return default;
+            }
         }
 
         public string GetAccountHistory()
