9a29cafa2b5c5fcf239e2ccfe19dda7be6be9fc7
FateevNIX <aleksandr.fateev@nixsolutions.com>
2020-12-11 13:36:07 +0200

    add classes project

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Пашка", 1001);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+            Console.WriteLine(account.Balance);
+        }
+    }
+}
